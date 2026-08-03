f4435b774d44824bad4aa2fdff7668e38aae3026
mjKas <manuja2zlato@gmail.com>
2026-08-03 15:50:12 +0530

    pom updated

---
+++ src/main/java/com/assignment/trainbookingsystem/TrainbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.trainbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainbookingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainbookingsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/trainbookingsystem/TrainbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.trainbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainbookingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
